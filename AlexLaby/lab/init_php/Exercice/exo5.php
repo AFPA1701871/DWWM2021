db6b905b890f4ef217103abd7e2cea13bc8f9f52
labya <alexlaby.al@gmail.com>
2021-01-07 09:12:25 +0100

    al_exo

--- AlexLaby/lab/init_php/Exercice/exo5.php
+++ AlexLaby/lab/init_php/Exercice/exo5.php
@@ -4,10 +4,10 @@
     if ($line>0){
         $reponse="nombre positif";
     }
-    if ($line<0){
+    else if ($line<0){
         $reponse="nombre négatif";
     }
-    if ($line=0){
+    else if ($line==0){
         $reponse="Votre nombre est 0";
     }
     echo $reponse;
