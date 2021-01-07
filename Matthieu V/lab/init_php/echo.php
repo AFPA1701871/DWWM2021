b0162ebdbc89c5f14b2e760532a20b3ce45e3002
Verhille Matthieu <matthieu.verhille@gmail.com>
2021-01-07 13:05:05 +0100

    MV_test php

---
+++ Matthieu V/lab/init_php/echo.php
@@ -0,0 +1,11 @@
+<?php
+   
+    $a="feminin";
+    $age=19;
+    switch($a){
+        case "feminin":
+                if($age>=18 && $age<=35){
+                    echo "impossable";
+                }
+    }
+?>
