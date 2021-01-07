bc0832e903552d384531437891016f37d54f0fc6
J-Wyts <jordan.wyts@yahoo.fr>
2021-01-07 23:46:09 +0100

    JW_MAJ PHP

--- JordanW/PHP/BONUS/morpion.php
+++ JordanW/PHP/BONUS/morpion.php
@@ -27,14 +27,12 @@
         $intScoreDiag = testDiagonale($tabMorpion);
         $intScoreCol = testColonne($tabMorpion);
         $intScoreLig = testLigne($tabMorpion);
-        echo "diag=".$intScoreDiag;
-        echo "col=".$intScoreCol;
-        echo "ligne=".$intScoreLig;
+
     //Personne n'a gagne et tour <9
     }while(($intScoreDiag+$intScoreCol+$intScoreLig == 0) OR ($intTour>=9));
     
     //Test qui gagne
-    echo testGagne($intScoreDiag,$intScoreColonne,$intScoreLigne);
+    echo testGagne($intScoreDiag,$intScoreCol,$intScoreLig);
 
 function testFini(&$tab)
 {
@@ -51,9 +49,9 @@ function testFini(&$tab)
 function testGagne($intscoreD,$intScoreC,$intScoreL){
     if($intscoreD==0 && $intScoreC==0 && $intScoreL==0){
         echo "EGALITE";    
-    }elseif ($intscoreD==10 OR $intScoreC==10 OR $intscoreL==10){
+    }elseif ($intscoreD==10 OR $intScoreC==10 OR $intScoreL==10){
         echo "Vous avez gagne !";
-    }elseif($intscoreD==20 OR $intScoreC==20 OR $intscoreL==20){
+    }elseif($intscoreD==20 OR $intScoreC==20 OR $intScoreL==20){
         echo "Vous avez perdu !";   
     }
 }
@@ -93,7 +91,6 @@ function initTab(&$tab)
 function remplirTab(&$tab,$ligne,$colonne,$car)
 {
     $tab[$ligne][$colonne] = $car;
-    echo "ligne ". $ligne . " colone ". $colonne . " car " . $car."\n";
     return $tab;
 }
 function afficherTab(&$tab)
@@ -110,19 +107,15 @@ function testDiagonale(&$tab)
     $resultat = 0;
     if(($tab[0][2] == $tab[1][1])  &&  ($tab[1][1] == $tab[2][0])){
         if($tab[0][2] == "x"){
-            echo "diag1";
            $resultat = 10;
          }elseif($tab[0][2] == "o"){
-            echo "diag1";
            $resultat = 20;
          }
    }elseif(($tab[2][2] == $tab[1][1])  && ($tab[1][1]=$tab[0][0])){
        if($tab[2][2] == "x"){
-            echo "diag 2",
             $resultat = 10;
        }elseif($tab[2][2] == "o"){
             $resultat = 20;
-            echo "diag2";
        }
    } 
 
