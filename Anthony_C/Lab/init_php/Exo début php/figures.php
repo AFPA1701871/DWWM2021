03124b4cc1ac11be9c81a2171c79d60c9b20865a
Consil-Anthony <consil.anthony@outlook.com>
2021-01-07 19:10:27 +0100

    Exo 7.1.2021

--- Anthony_C/Lab/init_php/Exo début php/figures.php
+++ Anthony_C/Lab/init_php/Exo début php/figures.php
@@ -1,11 +1,15 @@
 <?php
+    echo "Première figure :";
+
     $figure1="";
     for ($j=0; $j < 7; $j++) { 
-        $figure1=$figure1."*";
+        $figure1=$figure1."*"; /* Un "for" pour ajouter "*" chaque boucle.*/
         echo $figure1 . "\n";
     }
     for ($i=6; $i >0 ; $i--) { 
-        $figure2=substr($figure1,0,$i);
+        $figure2=substr($figure1,0,$i); /* Pareil que le premier "for" sauf qu'on retire le caractère positionner a l'emplacement "$i" de "$figure2".*/
         echo $figure2 . "\n";
     }
+
+    //echo "Deuxième figure :";  /* En cours de construction) */
 ?>
