9b329e6ae7f620b62dcd76cbac002ba4c26c86fb
Consil-Anthony <consil.anthony@outlook.com>
2021-01-08 08:16:32 +0100

    Exo 7.1.2021

--- Anthony_C/Lab/init_php/Exo début php/figures.php
+++ Anthony_C/Lab/init_php/Exo début php/figures.php
@@ -14,21 +14,20 @@
     echo "Deuxième figure :" . "\n";  /* En cours de construction) */
 
     $figure2="";
-    $count=3;
-    for ($i=0; $i <8; $i++) {
-        $figure2Len=strlen($figure2);
-        if ($figure2Len<=1) {
-            $figure2=$figure2."X";
+    for ($i=0; $i <8; $i++) {       /* Boucle $i jusqu'a la longueur de ma figure. */
+        $figure2Len=strlen($figure2);   /* Calculez la longueur de ma figure. */
+        if ($figure2Len<=1) {           /* Si la longueur de ma figure est < ou = a 1. */
+            $figure2=$figure2."X";      /* j'ajoute un "X". */
             echo $figure2 ."\n";
         }
-        elseif ($figure2Len<=6) {
-            $figure2Bis=substr($figure2,0,$figure2Len-1);
-            $figure2Bis=$figure2Bis."O"."X";
-            $figure2=$figure2Bis;
+        elseif ($figure2Len<=6) {   /* Sinon la longueur de ma figure est < ou = a 6. */
+            $figure2Bis=substr($figure2,0,$figure2Len-1);   /* Je retire la dernière valeur de ma figure. */
+            $figure2Bis=$figure2Bis."O"."X";    /* J'ajoute un "O" puis un "X" a ma figure. */
+            $figure2=$figure2Bis;   /* ici j'initialise ma figure avec ce que j'ai fais les deux lignes du dessus. */
             echo $figure2Bis."\n";      
         }
-        elseif ($figure2len=7) {
-            echo "XXXXXXXX";
+        elseif ($figure2len=7) {    /* Sinon la longueur de ma figure est = 7. */
+            echo "XXXXXXXX";    /* Ce qui termine ma boucle est ma figure. */
         }
     }
 ?>
