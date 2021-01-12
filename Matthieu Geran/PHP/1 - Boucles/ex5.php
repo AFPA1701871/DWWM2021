86f176ef716ab7813d38343e2c3e1df4c1f1d938
geranm <matt59210@gmail.com>
2021-01-12 08:41:20 +0100

    MG

--- Matthieu Geran/PHP/1 - Boucles/ex5.php
+++ Matthieu Geran/PHP/1 - Boucles/ex5.php
@@ -1,12 +1,13 @@
 <?php
 
-    
     $nb1 = -999999999;
     $nbPosition = 0;
 
     for ($i=1;$i<=5;$i++){
-        
-        $nb2 = readline("Tapez le nombre " . $i . " : ");
+
+        do{       
+            $nb2 = readline("Tapez le nombre " . $i . " : ");
+        } while ($nb2 <= 0 or $nb2 <> intdiv($nb2, 1));
         
         if ($nb2>$nb1){
             $nb1=$nb2;
