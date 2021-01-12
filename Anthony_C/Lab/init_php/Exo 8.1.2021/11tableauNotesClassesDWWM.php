7b439fa160ba7aad9b49e5b738bf97d67ee8cc00
Consil-Anthony <consil.anthony@outlook.com>
2021-01-12 17:34:11 +0100

    Exo 12.1.2021

--- Anthony_C/Lab/init_php/Exo 8.1.2021/11tableauNotesClassesDWWM.php
+++ Anthony_C/Lab/init_php/Exo 8.1.2021/11tableauNotesClassesDWWM.php
@@ -1,18 +1,38 @@
 <?php
-    $tabNotes[0]=array("clé","valeur");
-    $tabNotes[1]=array("Franck",14);
-    $tabNotes[2]=array("Kesary",16);
-    $tabNotes[3]=array("Jimmy",15);
-    afficherTab($tabNotes);
+    //Création du tableau.
+    $tabNotes=array("Clé" => "Valeur","Franck" => 14,"Kesary" => 16,"Jimmy" => 15);
+    print_r($tabNotes);
+    echo "\n";
+
+    //Ajouter houssam et sa notes au tableau.
+    $tabNotes["Houssam"]=15;
+    print_r($tabNotes);
+    echo "\n";
     
+    //Retirer la note de Franck. 
+    $tabNotes["Franck"]="";
+    print_r($tabNotes);
+    echo "\n";
+
+    //Déterminer la note maximale et la note minimale.
+    $min = min($tabNotes["Kesary"],$tabNotes["Jimmy"],$tabNotes["Houssam"]);
+    echo "La note la plus petite est : $min\n";
+    $max = max($tabNotes["Kesary"],$tabNotes["Jimmy"],$tabNotes["Houssam"]);
+    echo "La note la plus grande est : $max\n";
+    echo "\n";
+
+    //Afficher le tableau après l'avoir trié par ordre alpha.
+    ksort($tabNotes);
+    print_r($tabNotes);
+    echo "\n";
+
+    //Afficher le tableau après l'avoir trié par ordre de mérite.
     
-    
-    function afficherTab(array $tab){
-        foreach ($tab as $l) {
-            foreach ($l as $c){
-                echo $c." ";
-            }
-            echo "\n";
-        }
-        }
+    print_r($tabNotes);
+    echo "\n";
+
+    //Moyennes des notes.
+    $moyenne = array_sum($tabNotes)/3;
+    echo "La moyenne des notes est de $moyenne."
+
 ?>
