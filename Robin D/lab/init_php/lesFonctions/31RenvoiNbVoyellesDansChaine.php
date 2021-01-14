7754a51668cd7c3fc592ca63907fd36fefaeb422
RobinDesm <robin.desmoudt@gmail.com>
2021-01-14 09:14:06 +0100

    RD_ajoutDesExercicesInitPHP14

--- Robin D/lab/init_php/lesFonctions/31RenvoiNbVoyellesDansChaine.php
+++ Robin D/lab/init_php/lesFonctions/31RenvoiNbVoyellesDansChaine.php
@@ -1,14 +1,18 @@
 <?php
     echo " **** Renvoi du Nombre de voyelles dans une phrase ****\n";
 
+    // fonction qui permet de renvoyer le nombre de voyelles de la chaine $maPhrase
     function compterNbVoyelles($maPhrase) {
         $maPhraseEnMinuscule = strtolower($maPhrase);
         $tableauMaPhrase=str_split($maPhraseEnMinuscule);
+        $tableauVoyelles=array("a","e","i","o","u","y");
 
         $nbVoyelles=0;
         for ($i=0; $i < count($tableauMaPhrase); $i++) { 
-            if ($tableauMaPhrase[$i]=="a" or $tableauMaPhrase[$i]=="e" or $tableauMaPhrase[$i]=="i" or $tableauMaPhrase[$i]=="o" or $tableauMaPhrase[$i]=="u" or $tableauMaPhrase[$i]=="y") {
-                $nbVoyelles=$nbVoyelles+1;
+            for ($j=0; $j < count($tableauVoyelles); $j++) { 
+                if ($tableauMaPhrase[$i]==$tableauVoyelles[$j]) {
+                    $nbVoyelles=$nbVoyelles+1;
+                }
             }
         }
         return $nbVoyelles;
---
+++ Robin D/lab/init_php/lesFonctions/33PurgeChaineDUnOuPlusieursCaractere.php
@@ -0,0 +1,28 @@
+<?php
+    echo " **** Purge la chaine d'un ou plusieurs caractères ****\n";
+    
+    // fonction qui permet de purger la chaine $phrase des $caracteres de la chaine $CaracteresAPurger
+    function purgeChaineDUncaractere($phrase,$CaracteresAPurger) {
+        $maPhraseEnMinuscule = strtolower($phrase);
+        $tableauMaPhrase=str_split($maPhraseEnMinuscule);
+        $CaractereAPurgerEnMinuscule=strtolower($CaracteresAPurger);
+        $tableauCaracteresPurger=str_split($CaractereAPurgerEnMinuscule);
+        
+
+        for ($i=0; $i < count($tableauMaPhrase); $i++) { 
+            for ($j=0; $j < count($tableauCaracteresPurger); $j++) { 
+                if ($tableauMaPhrase[$i]==$tableauCaracteresPurger[$j]) {
+                    $tableauMaPhrase[$i]="";
+                }
+            }
+        }
+        print_r($tableauMaPhrase);
+        $phrasePurgee=implode($tableauMaPhrase);
+        return $phrasePurgee;
+    }
+
+    $maChaine=readline("entrez une phrase :");
+    $mesCaracteresAPurger=readline("entrez un ou plusieurs caractères à purger (en collant toutes les lettres) :");
+
+    echo "la phrase $maChaine purgée des caractères $mesCaracteresAPurger est : ".purgeChaineDUncaractere($maChaine,$mesCaracteresAPurger);
+?>
