988cbbcb129f72cf7e7b32ff3c1def12ae0fb2b1
labya <alexlaby.al@gmail.com>
2021-01-15 19:04:15 +0100

    al_exo

--- AlexLaby/Projet_SGBD/src/index.php
+++ AlexLaby/Projet_SGBD/src/index.php
@@ -1,11 +1,11 @@
 <?php
+
 require('./service/Librairie_fonc.php');
 
 initConfig();
 
 $inputLogin=readline("Entrez votre identifiant : ");
 $inputPassword=readline("Entrez votre mot de passe : ");
-
-
+login($inputLogin,$inputPassword);
 
 ?>
