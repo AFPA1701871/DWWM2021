978df9e9b6a8165ae66eba1326947e55a3667ca5
Laurent-62100 <leroux.laurent62@hotmail.fr>
2021-01-19 10:58:03 +0100

    LL_projet

--- Laurent L/Projet SGBD/src/service/verifications.php
+++ Laurent L/Projet SGBD/src/service/verifications.php
@@ -1,32 +1,48 @@
 <?php
 
 function authentification($string){
-    $auth = false;
     $fp = fopen("../BDD/config.ini","r");
     while(!feof($fp)){
         //pour prendre ligne apres ligne
         $ligne = fgets($fp,50);        
         //pour couper la ligne en donnée unitaire dans un tableau
-        // $tabLine = explode(";",$ligne);   
+        $tabLine = explode(";",$ligne);   
         //concatener les données pour recreer une identification d'un user
-        // $ligne = $tabLine[0].";".$tabLine[1]; 
-
+        $ligne = $tabLine[0].";".$tabLine[1]; 
         if($string == $ligne){
             $auth = true;
             break;
         }   
-        // echo $string." ".$ligne." ".$auth;   
-        // var_dump($string);
-        // var_dump($ligne);    
-        // $var = readline('quest');  
     }
     fclose($fp);
-   
     return $auth;
-
 }
 
+function verifSaisie($saisie){
+    $tabSaisie = explode(" ",$saisie);
+    $tabSaisie[0] = strtoupper($tabSaisie[0]);
+    $tabSaisie[1] = strtoupper($tabSaisie[1]);
 
+
+
+    switch ($saisie){
+        case ($tabSaisie[0]=="CREATE" and $tabSaisie[1]=="TABLE"):
+            echo "CREATE TABLE ok";
+            
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
