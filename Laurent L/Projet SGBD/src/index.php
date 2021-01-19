7b00afccf34f9695a13edf4e8a68df9d006d7e45
Consil-Anthony <consil.anthony@outlook.com>
2021-01-19 10:36:48 +0100

    Exo Projet

--- Laurent L/Projet SGBD/src/index.php
+++ Laurent L/Projet SGBD/src/index.php
@@ -1,34 +1,15 @@
 <?php
     //variables
     require('service/verifications.php');
-    $auth = false;
 
 
     //authentification
-    while ($auth==false){
+    do{
         $login = readline("Login    : ");
         $password = readline("Password : ");
         $user = $login.";".$password;
-        $fp = fopen("../BDD/config.ini","r");
-        while(!feof($fp)){
-            //pour prendre ligne apres ligne
-            $ligne = fgets($fp,50);        
-            //pour couper la ligne en donnée unitaire dans un tableau
-            $tabLine = explode(";",$ligne);   
-            //concatener les données pour recreer une identification d'un user
-            $ligne = $tabLine[0].";".$tabLine[1]; 
-            if($user == $ligne){
-                $auth = true;
-            }   
-            // echo $user." ".$ligne." ".$auth;   
-            // var_dump($user);
-            // var_dump($ligne);      
-        }
-        if(!$auth){
-            echo "Identification incorrecte.\n";
-        }       
-        fclose($fp);
-    }
+        $auth=login($user);
+    }while ($auth==false);
     
     $commande = readline("SQL :>");
     $commande = strtolower($commande);
--- Laurent L/Projet SGBD/src/service/verifications.php
+++ Laurent L/Projet SGBD/src/service/verifications.php
@@ -1,5 +1,56 @@
 <?php
 
+function login($string){
+    $fp = fopen("../BDD/config.ini","r");
+        $auth = false;
+        while(!feof($fp)){
+            //pour prendre ligne apres ligne
+            $ligne = fgets($fp,50);        
+            //pour couper la ligne en donnée unitaire dans un tableau
+            $tabLine = explode(";",$ligne);   
+            //concatener les données pour recreer une identification d'un user
+            $ligne = $tabLine[0].";".$tabLine[1]; 
+            if($string == $ligne){
+                $auth = true;
+            }   
+            // echo $user." ".$ligne." ".$auth;   
+            // var_dump($user);
+            // var_dump($ligne);      
+        }
+        if(!$auth){
+            echo "Identification incorrecte.\n";
+        }       
+        fclose($fp);
+        return $auth;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // analyse de la saisie et recupération des variables de traitement
 
 function analyse($commande){
