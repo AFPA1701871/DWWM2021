09f9b2c6b84d3940da742fc8f31be8076a78d0cb
Laurent-62100 <leroux.laurent62@hotmail.fr>
2021-01-19 08:59:52 +0100

    LL_projet

--- Laurent L/Projet SGBD/src/index.php
+++ Laurent L/Projet SGBD/src/index.php
@@ -1,7 +1,7 @@
 <?php
     //variables
     require('service/verifications.php');
-    $auth = false;
+    // $auth = false;
 
 
     //authentification
@@ -9,25 +9,28 @@
         $login = readline("Login    : ");
         $password = readline("Password : ");
         $user = $login.";".$password;
-        $fp = fopen("../BDD/config.ini","r");
-        while(!feof($fp)){
-            //pour prendre ligne apres ligne
-            $ligne = fgets($fp,50);        
-            //pour couper la ligne en donnée unitaire dans un tableau
-            $tabLine = explode(";",$ligne);   
-            //concatener les données pour recreer une identification d'un user
-            $ligne = $tabLine[0].";".$tabLine[1]; 
-            if($user == $ligne){
-                $auth = true;
-            }   
-            // echo $user." ".$ligne." ".$auth;   
-            // var_dump($user);
-            // var_dump($ligne);      
-        }
+        // $fp = fopen("../BDD/config.ini","r");
+        // while(!feof($fp)){
+        //     //pour prendre ligne apres ligne
+        //     $ligne = fgets($fp,50);        
+        //     //pour couper la ligne en donnée unitaire dans un tableau
+        //     $tabLine = explode(";",$ligne);   
+        //     //concatener les données pour recreer une identification d'un user
+        //     $ligne = $tabLine[0].";".$tabLine[1]; 
+        //     if($user == $ligne){
+        //         $auth = true;
+        //     }   
+        //     // echo $user." ".$ligne." ".$auth;   
+        //     // var_dump($user);
+        //     // var_dump($ligne);      
+        // }
+        // fclose($fp);
+        
+        authentification($user);
         if(!$auth){
             echo "Identification incorrecte.\n";
         }       
-        fclose($fp);
+        
     }
     
     $commande = readline("SQL :>");
--- Laurent L/Projet SGBD/src/service/verifications.php
+++ Laurent L/Projet SGBD/src/service/verifications.php
@@ -1,50 +1,28 @@
 <?php
 
-// analyse de la saisie et recupération des variables de traitement
+function authentification($string){
+    $auth = false;
+    $fp = fopen("../BDD/config.ini","r");
+    while(!feof($fp)){
+        //pour prendre ligne apres ligne
+        $ligne = fgets($fp,50);        
+        //pour couper la ligne en donnée unitaire dans un tableau
+        // $tabLine = explode(";",$ligne);   
+        //concatener les données pour recreer une identification d'un user
+        // $ligne = $tabLine[0].";".$tabLine[1]; 
 
-function analyse($commande){
-    $tabCommande = explode(" ",$commande);
-    if ($tabCommande[0]=="create" && $tabCommande[1]=="table"){
-        $instruction =$tabCommande[0].$tabCommande[1];
-        $fichier = $tabCommande[2];
-        if ((strpos($fichier,"(") && strpos($fichier,")") && strpos($fichier,","))){
-            $tabFichier = explode("(",$fichier);
-            $nomFichier = $tabFichier[0];
-            $enteteFichier = $tabFichier[1];
-            $enteteFichier = str_replace(",",";",$enteteFichier);
-            $enteteFichier = str_replace(")",";",$enteteFichier);
-        } else{
-            $erreurSyntaxe = true;
-        }
-        
-    } else if($tabCommande[0]=="insert" && $tabCommande[1]=="into" && (substr($tabCommande[3],0,5)=="values")){
-        $instruction = $tabCommande[0].$tabCommande[1];
-        $nomFichier = $tabCommande[2];
-        if ((strpos($tabCommande[3],"(") && strpos($tabCommande[3],")") && strpos($tabCommande[3],",") && strpos($tabCommande[3],"'"))){
-            $tabFichier = explode("(",$tabCommande[3]);
-            $enteteFichier = $tabFichier[1];
-            $enteteFichier = str_replace("'","",$enteteFichier);
-            $enteteFichier = str_replace(",",";",$enteteFichier);
-            $enteteFichier = str_replace(")",";",$enteteFichier);
-        } else{
-            $erreurSyntaxe = true;
-        }
-    } else if($tabCommande[0]=="select" && $tabCommande[1]=="*" && $tabCommande[2]=="from"){
-        print_r($tabCommande);
-        $instruction = $tabCommande[0].$tabCommande[1].$tabCommande[2];
-        $nomFichier = $tabCommande[3];
-    } else{
-        $erreurSyntaxe = true;
+        if($string == $ligne){
+            $auth = true;
+            break;
+        }   
+        // echo $string." ".$ligne." ".$auth;   
+        // var_dump($string);
+        // var_dump($ligne);    
+        // $var = readline('quest');  
     }
-
-    // if ($enteteFichier!=""){
-    //     $donneesTraitement = [$instruction, $nomFichier, $enteteFichier];
-    // }else {
-    //     $donneesTraitement = [$instruction, $nomFichier] ;
-    // }
-
-
-
+    fclose($fp);
+   
+    return $auth;
 
 }
 
