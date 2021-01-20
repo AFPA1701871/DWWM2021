c427769f93db39f711331e107d5bafc784f55cae
Consil-Anthony <consil.anthony@outlook.com>
2021-01-20 18:41:39 +0100

    Project

--- Anthony_C/projet_SGBD/src/function.php
+++ Anthony_C/projet_SGBD/src/function.php
@@ -4,7 +4,9 @@
         $fp = fopen("..\BDD\config.ini","r");
         $boolean = false;
         $compteur = 0;
+        
         while (!feof($fp)) {
+            //Explode the line "n" of the file and test if the login / password is correct.
             $authentificationTab=explode(";", fgets($fp, 4096));
             if ($compteur != 0){
                 if($authentificationTab[0]==$login and $authentificationTab[1]==$password){
@@ -12,7 +14,8 @@
                 }
             }
             $compteur++;
-        }   
+        }
+        //If the test is incorrect write this message and ask a login / password again.   
         if($boolean == false){
             echo "Error login/password incorrect, please try again.\n";
         }
@@ -23,12 +26,14 @@
     //function when the user inputs is quit stop the application.
     function quitApplication($value){
         if ($value == "quit"){
+            //quit the application.
             exit;
         }
     }
 
     //function test all the inputs of the user.
     function testInputs($value){
+            //explode the value and try all case if it's wrong ask the value again.
             $valueTab = explode(" ",$value);
             switch ($value){
                 case "help":
@@ -59,17 +64,21 @@
 
     //function create.
     function createTable($value){
+        //Check if the file is already created or not.
         $newFileName="..\BDD\\".substr($value,13,(strpos($value,"(")-13)).".DWWM";
         if(file_exists($newFileName)){
             echo substr($value,13,(strpos($value,"(")-13))." already exist.\n";
         }else{
+            //If the file isn't created explode the value between "()".
             $testCharTab=explode(",",substr($value,strpos($value,"(")+1,(strpos($value,")")-iconv_strlen($value))));
             $bool = true;
+            //Test if each value in the table is >=25.
             foreach ($testCharTab as $test){
                 if (iconv_strlen($test) >= 25){
                     $bool = false;
                 }
             }
+            //Check if one of the value is not written twice.
             for ($i=0;$i<count($testCharTab);$i++){
                 for ($j=$i+1;$j<count($testCharTab);$j++){
                     if ($testCharTab[$i] == $testCharTab[$j]){
@@ -77,12 +86,14 @@
                     }
                 }
             }
+            //If everything is good so the function create the file and replace all "," by ";".
             if ($bool == true){
                 $fp = fopen($newFileName,"w");
                 $value=str_replace(",",";",$value);
                 fputs($fp, substr($value,strpos($value,"(")+1,(strpos($value,")")-iconv_strlen($value))).";");
                 fclose($fp);
                 echo substr($value,13,(strpos($value,"(")-13))." is created.\n";
+            //Or ask the value again because it's not correct.
             }else{
                 echo "Too many characters in a column.\n"."Reminder : you can have the same values in your column.\n";
             }
@@ -91,11 +102,14 @@
 
     //function Insert into.
     function insertInto($value){
+        //Check if the file exist or not.
         $fileName="..\BDD\\".substr($value,12,(strpos(strtoupper($value),"VALUES")-13)).".DWWM";
         if(file_exists($fileName)){
+            //If the file exist then open it and explode the user values into a tab.
             $fp = fopen($fileName, "r");
             $valueTab = explode(",",substr($value,strpos($value,"(")+1,(strpos($value,")")-iconv_strlen($value))));
             $count=0;
+            //Explode the first line of the file.
             while(!feof($fp)){
                 if ($count == 0){
                     $nbColoneTab=explode(";", fgets($fp,4096));
@@ -103,14 +117,17 @@
             }
             fclose($fp);
             $bool = true;
+            //Test each user values if they're not over 25 char.
             foreach ($valueTab as $test){
                 if (iconv_strlen($test) >= 25){
                     $bool = false;
                 }
             }
+            //If everything is good insert the user values in the file.
             if (count($valueTab)+1 == count($nbColoneTab) and $bool == true){
                 $fp = fopen($fileName,"a");
                 fputs($fp,"\n");
+                //Replace all "," into ";" and remove all "'".
                 $value=str_replace(",",";",$value);
                 $value=str_replace("'","",$value);
                 fputs($fp,substr($value,strpos($value,"(")+1,(strpos($value,")")-iconv_strlen($value))).";");
@@ -125,50 +142,59 @@
 
     //function help user.
     function help($value){
-        echo "Cheh t'auras pas d'aide!\n";
+        //what did you expect ? ¯\_(ツ)_/¯
+        echo "No help for you sorry.\n";
     }
 
     //function select * from.
     function selectAllFrom($value){
+        //Check if the file exist or not.
         $newFileName="..\BDD\\".substr($value,14,strpos($value,";")-14).".DWWM";
         if(file_exists($newFileName)){
             $fp = fopen("..\BDD\\".substr($value,14,strpos($value,";")-14).".DWWM","r");
             $count=0;
-            $cadre="+";
+            $frame="+";
+            //Create a tab with all values from the file.
             while (!feof($fp)){
                 $showTab[$count]=explode(";", fgets($fp, 4096));
                 $count++;
             }
             fclose($fp);
-            for ($i=0;$i<count($showTab[0])-1;$i++){  //$i == ligne
-                $plusGrand="";
-                for ($j=0;$j<count($showTab)-1;$j++){  //$j == colonne
+            //loop on the tab and get the longuest values from each column.
+            for ($i=0;$i<count($showTab[0])-1;$i++){  //$i = line
+                $high="";
+                for ($j=0;$j<count($showTab)-1;$j++){  //$j = column
                     if (iconv_strlen($showTab[$j][$i])<iconv_strlen($showTab[$j+1][$i])){
-                        $plusGrand=$showTab[$j+1][$i];
+                        $high=$showTab[$j+1][$i];
                     }else if(iconv_strlen($showTab[$j][$i])>iconv_strlen($showTab[$j+1][$i])){
-                        $plusGrand=$showTab[$j][$i];
+                        $high=$showTab[$j][$i];
                     }
                 }
+                //loop on the tab and add a " " until the value have the same number of char. 
                 for ($j=0;$j<count($showTab);$j++){
-                    while(iconv_strlen($showTab[$j][$i])<=iconv_strlen($plusGrand)){
+                    while(iconv_strlen($showTab[$j][$i])<=iconv_strlen($high)){
                         $showTab[$j][$i].=" ";
                     }
+                    //Add a "|" at the end of each values and stock the values.
                     $showTab[$j][$i].="|";
                     $stock=$showTab[$j][$i];
+                    //Add a "| " on each values in the first column and only a " " for the rest before the value.
                     if ($i==0){
                         $showTab[$j][$i]="| ".$showTab[$j][$i];
                     }else{
                         $showTab[$j][$i]=" ".$showTab[$j][$i];
                     }
                 }
+                //Custom the frame with the longuest values for each column.
                 for ($k=0;$k<iconv_strlen($stock)+2;$k++){
                     if($k==iconv_strlen($stock)){
-                        $cadre.="+";
+                        $frame.="+";
                     }else if($k<iconv_strlen($stock)){
-                        $cadre.="-";
+                        $frame.="-";
                     }
                 }
             }
+            //Display the tab.
             for($i=0;$i<count($showTab);$i++){
                 $var="";
                 for($j=0;$j<(count($showTab[$i]));$j++){
@@ -177,12 +203,12 @@
                 if($i==0 or $i==1 or $i==count($showTab)-1){
                     if($i==count($showTab)-1){
                         echo $var."\n";
-                        echo $cadre."\n";
+                        echo $frame."\n";
                     }else if($i==1){
-                        echo $cadre."\n";
+                        echo $frame."\n";
                         echo $var;
                     }else{
-                        echo $cadre."\n";
+                        echo $frame."\n";
                         echo $var;
                     }
                 }else{
