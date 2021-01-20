60143cc074762b331eee30c7e9d7f6d760aa44bb
Consil-Anthony <consil.anthony@outlook.com>
2021-01-20 18:00:01 +0100

    Projet

--- Anthony_C/projet_SGBD/src/function.php
+++ Anthony_C/projet_SGBD/src/function.php
@@ -43,9 +43,9 @@
                 case strtoupper($valueTab[0]) == "INSERT" and strtoupper($valueTab[1]) == "INTO" and strtoupper(substr($valueTab[3],0,6)) == "VALUES" and substr($value,-1,1) == ";" and strpos($valueTab[3],"(") >= 1 and strpos($valueTab[3],")") >=1:
                     insertInto($value);
                     break;
-                case strtoupper($valueTab[0]) == "SELECT" and strtoupper($valueTab[2]) == "FROM" and substr($value,-1,1) == ";" and strpos($valueTab[2],"(") < 1 and strpos($valueTab[2],")") < 1:
+                case strtoupper($valueTab[0]) == "SELECT" and strtoupper($valueTab[2]) == "FROM" and substr($value,-1,1) == ";" and strpos($valueTab[3],"(") < 1 and strpos($valueTab[3],")") < 1:
                     if($valueTab[1] == "*"){
-                        selectAllFrom();
+                        selectAllFrom($value);
                     }else{
                         selectValueFrom();
                     }
@@ -63,20 +63,28 @@
         if(file_exists($newFileName)){
             echo substr($value,13,(strpos($value,"(")-13))." already exist.\n";
         }else{
-            $testCharTab=explode(",",substr($value,strpos($value,"(")+1,(strpos($value,")")-strlen($value))));
+            $testCharTab=explode(",",substr($value,strpos($value,"(")+1,(strpos($value,")")-iconv_strlen($value))));
             $bool = true;
             foreach ($testCharTab as $test){
-                if (strlen($test) >= 25){
+                if (iconv_strlen($test) >= 25){
                     $bool = false;
                 }
             }
+            for ($i=0;$i<count($testCharTab);$i++){
+                for ($j=$i+1;$j<count($testCharTab);$j++){
+                    if ($testCharTab[$i] == $testCharTab[$j]){
+                        $bool = false;
+                    }
+                }
+            }
             if ($bool == true){
-            $fp = fopen($newFileName,"w");
-            fputs($fp, substr($value,strpos($value,"(")+1,(strpos($value,")")-strlen($value))));
-            fclose($fp);
-            echo substr($value,13,(strpos($value,"(")-13))." is created.\n";
+                $fp = fopen($newFileName,"w");
+                $value=str_replace(",",";",$value);
+                fputs($fp, substr($value,strpos($value,"(")+1,(strpos($value,")")-iconv_strlen($value))).";");
+                fclose($fp);
+                echo substr($value,13,(strpos($value,"(")-13))." is created.\n";
             }else{
-                echo "Too many characters in a column.\n";
+                echo "Too many characters in a column.\n"."Reminder : you can have the same values in your column.\n";
             }
         }
     }
@@ -86,21 +94,29 @@
         $fileName="..\BDD\\".substr($value,12,(strpos(strtoupper($value),"VALUES")-13)).".DWWM";
         if(file_exists($fileName)){
             $fp = fopen($fileName, "r");
-            $valueTab = explode(",",substr($value,strpos($value,"(")+1,(strpos($value,")")-strlen($value))));
+            $valueTab = explode(",",substr($value,strpos($value,"(")+1,(strpos($value,")")-iconv_strlen($value))));
             $count=0;
             while(!feof($fp)){
                 if ($count == 0){
-                    $nbColoneTab=explode(",", fgets($fp,4096));
+                    $nbColoneTab=explode(";", fgets($fp,4096));
                 }
             }
             fclose($fp);
-            if (count($valueTab) == count($nbColoneTab)){
+            $bool = true;
+            foreach ($valueTab as $test){
+                if (iconv_strlen($test) >= 25){
+                    $bool = false;
+                }
+            }
+            if (count($valueTab)+1 == count($nbColoneTab) and $bool == true){
                 $fp = fopen($fileName,"a");
                 fputs($fp,"\n");
-                fputs($fp,substr($value,strpos($value,"(")+1,(strpos($value,")")-strlen($value))));
+                $value=str_replace(",",";",$value);
+                $value=str_replace("'","",$value);
+                fputs($fp,substr($value,strpos($value,"(")+1,(strpos($value,")")-iconv_strlen($value))).";");
                 fclose($fp);
             }else{
-                echo "Invalid inputs, you need to enter ".count($nbColoneTab)." inputs after in the '()' séparated by ','.\n";
+                echo "Invalid inputs, you need to enter ".count($nbColoneTab)." inputs after in the '()' séparated by ',' or atleast one of your value is more than 25 char.\n";
             }
         }else{
             echo substr($value,12,(strpos(strtoupper($value),"VALUES")-13))." doesn't exist.\n";
@@ -110,5 +126,71 @@
     //function help user.
     function help($value){
         echo "Cheh t'auras pas d'aide!\n";
+    }
+
+    //function select * from.
+    function selectAllFrom($value){
+        $newFileName="..\BDD\\".substr($value,14,strpos($value,";")-14).".DWWM";
+        if(file_exists($newFileName)){
+            $fp = fopen("..\BDD\\".substr($value,14,strpos($value,";")-14).".DWWM","r");
+            $count=0;
+            $cadre="+";
+            while (!feof($fp)){
+                $showTab[$count]=explode(";", fgets($fp, 4096));
+                $count++;
+            }
+            fclose($fp);
+            for ($i=0;$i<count($showTab[0])-1;$i++){  //$i == ligne
+                $plusGrand="";
+                for ($j=0;$j<count($showTab)-1;$j++){  //$j == colonne
+                    if (iconv_strlen($showTab[$j][$i])<iconv_strlen($showTab[$j+1][$i])){
+                        $plusGrand=$showTab[$j+1][$i];
+                    }else if(iconv_strlen($showTab[$j][$i])>iconv_strlen($showTab[$j+1][$i])){
+                        $plusGrand=$showTab[$j][$i];
+                    }
+                }
+                for ($j=0;$j<count($showTab);$j++){
+                    while(iconv_strlen($showTab[$j][$i])<=iconv_strlen($plusGrand)){
+                        $showTab[$j][$i].=" ";
+                    }
+                    $showTab[$j][$i].="|";
+                    $stock=$showTab[$j][$i];
+                    if ($i==0){
+                        $showTab[$j][$i]="| ".$showTab[$j][$i];
+                    }else{
+                        $showTab[$j][$i]=" ".$showTab[$j][$i];
+                    }
+                }
+                for ($k=0;$k<iconv_strlen($stock)+2;$k++){
+                    if($k==iconv_strlen($stock)){
+                        $cadre.="+";
+                    }else if($k<iconv_strlen($stock)){
+                        $cadre.="-";
+                    }
+                }
+            }
+            for($i=0;$i<count($showTab);$i++){
+                $var="";
+                for($j=0;$j<(count($showTab[$i]));$j++){
+                        $var.=$showTab[$i][$j];
+                }
+                if($i==0 or $i==1 or $i==count($showTab)-1){
+                    if($i==count($showTab)-1){
+                        echo $var."\n";
+                        echo $cadre."\n";
+                    }else if($i==1){
+                        echo $cadre."\n";
+                        echo $var;
+                    }else{
+                        echo $cadre."\n";
+                        echo $var;
+                    }
+                }else{
+                    echo $var;
+                } 
+            }
+        }else{
+            echo substr($value,14,strpos($value,";")-14)." doesn't exist.\n";
+        }
     }
 ?>
