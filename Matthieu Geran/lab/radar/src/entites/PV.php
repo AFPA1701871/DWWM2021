71cdbd27c1f4cefc2576a6a45a1fe6e169b47b4b
geranm <matt59210@gmail.com>
2021-01-25 00:06:45 +0100

    MG

---
+++ Matthieu Geran/lab/radar/src/entites/PV.php
@@ -0,0 +1,15 @@
+<?php
+
+    class PV{
+
+        private $immatriculation;
+        private $vitesse;
+
+        public function ecriture($immatriculation,$vitesse){
+
+        }
+        
+        
+    }
+
+?>
