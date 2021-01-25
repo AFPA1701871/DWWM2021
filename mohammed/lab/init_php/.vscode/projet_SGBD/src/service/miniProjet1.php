e6294dfb9ef4170678d5d0b781e65fcdf2bd40db
59011-85-12 <59011-85-12@exchange.ad.afpanet>
2021-01-25 08:39:49 +0100

    mb

--- mohammed/lab/init_php/.vscode/projet_SGBD/src/service/miniProjet1.php
+++ mohammed/lab/init_php/.vscode/projet_SGBD/src/service/miniProjet1.php
@@ -1,7 +1,6 @@
 <?php
 
-$fp =fopen("C:\git\mohammed\DWWM2021\mohammed\lab\init_php\.vscode\projet_SGBD\BDD\config.ini.txt", "r");
-$line=fgets($fp);
+$fp =fopen("..\..\BDD\config.ini.txt", "r");
 echo " veuillez saisir vos identifiant " ."\n";
 $i=0;
 do {
@@ -34,28 +33,6 @@ do {
                        echo $ok1;
 }   while ($ok==0 or $ok1==0); 
 $fp =fclose($fp);
-$fp1= fopen("C:\git\mohammed\DWWM2021\mohammed\lab\init_php\.vscode\projet_SGBD\src\service\data.txt.txt","a");
-$table=[" - nom  prenom - "," - numero - "," - email - "];
-$envoi="-nom --telephone--email--"."\n";
-
-fputs($fp1,$envoi,)."\n";
-$createtable = readline( " SQL :> ");
-$nom=readline( " entrez nom  ")."\n";
-fputs($fp1,$nom)."\n";
-$telephone=readline( " entrez tel  ")."\n";
-fputs($fp1,$telephone)."\n";
-$email=readline( " entrez email ")."\n";
-fputs($fp1,$email)."\n";
-for ($i=0; $i <10 ; $i++) {
-    echo $tab1[$i]="-"."\n";
-    for ($j=0; $j <10 ; $j++) { 
-        echo $tab1[$i][$j]="-";
-    }    
-}
-
-
-fputs($fp1,$sql);
-
 
 
 ?>
