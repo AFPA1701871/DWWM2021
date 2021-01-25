3779c554325db49842a346737c8e735ea3f83999
59011-85-02 <59011-85-02@exchange.ad.afpanet>
2021-01-25 17:24:30 +0100

    LL_POO

--- Laurent L/lab/POO/radar/entities/Voiture.class.php
+++ Laurent L/lab/POO/radar/entities/Voiture.class.php
@@ -3,18 +3,40 @@
         //attributs
         private $_marque;
         private $_modele;
-        private $immatriculation;
-        private $couleur;
-        private $vitesse;
-
-
-
+        private $_immatriculation;
+        private $_couleur;
+        private $_vitesse;
+        private $_acceleration;
 
         //constructeur
-
-
+        public function __construct($marque, $modele, $immat, $couleur){
+            $this->set_marque($marque);
+            $this->set_modele($modele);
+            $this->set_immatricutalion($immat);
+            $this->set_couleur($couleur);
+        }
 
         //methodes
+
+            //demarrage
+        public function demarrage(){
+            $this->set_vitesse(0);
+            //echo $this->get_vitesse();
+        }
+
+            //acceleration
+        public function acceleration(){
+            $acceleration = intval(rand(0,25));
+            $this->set_acceleration($acceleration);
+            $vitesse = $this->_vitesse + $this->_acceleration;
+            $this->set_vitesse($vitesse);
+            
+        }
+
+            //affichage acceleration et vitesse
+        public function affichageAccelVitesse(){
+            return "\nLa voiture immatriculée ".$this->_immatriculation.", de marque ".$this->_marque.", de type ".$this->_modele." et de couleur ".$this->_couleur." vient d'accélérer de ".$this->_acceleration." km/h et roule maintenant à ".$this->_vitesse." km/h.";
+        }
         
 
 
@@ -59,6 +81,13 @@
             $this->_vitesse = $_vitesse;
         }
 
+        public function get_acceleration(){
+            return $this->_acceleration;
+        }
+
+        public function set_acceleration($_acceleration){
+            $this->_acceleration = $_acceleration;
+        }
         
         
 
--- Laurent L/lab/POO/radar/src/index.php
+++ Laurent L/lab/POO/radar/src/index.php
@@ -1,13 +1,57 @@
 <?php
-
+    //variables
     require ("../service/fonction.php");  //liaison avec fichier fonction
     spl_autoload_register("chargerClasse");  //liaison avec toutes les classes
+    $infraction = false;
 
-    $voiture1 = new Voiture();
-    $voiture1->set_marque("audi");
+    //Limitation
+    do {
+        $limitation = intval(readline ("fixer la limite de vitesse : "));
+    }while ($limitation<50 || $limitation>130);
 
+    $radar = new Radar($limitation);
+    
 
-    echo "La marque de votre voiture est : ".$voiture1->get_marque();
+    //Nombre de voiture particitante
+    do{
+        $nbreVoitures = intval(readline("Nombre de voitures participantes : "));
+    }while ($nbreVoitures<2);
+    
+
+    //Création des voitures
+    for($i=1 ;$i<=$nbreVoitures; $i++){
+        echo "Renseignement du véhicule ".$i."\n";
+        $marque = readline("La marque du véhicule : ");
+        $modele = readline("Le modele du véhicule : ");
+        $immat = readline("L'immatriculation du véhicule : ");
+        $couleur = readline("La couleur du véhicule : ");
+        $voiture = "voiture$i";
+        $$voiture = new Voiture($marque, $modele, $immat, $couleur);
+        
+    }
+
+    //démarrage voiture initialisation vitesse à 0
+    for ($i = 1; $i<=$nbreVoitures; $i++){
+        $voiture = "voiture$i";
+        $$voiture->demarrage();
+    }
+
+    //acceleration de chaque voiture
+    while($infraction==false){
+        for($i=1;$i<=$nbreVoitures;$i++){
+            $voiture = "voiture$i";
+            $$voiture->acceleration();
+            echo $$voiture->affichageAccelVitesse();
+            if ($$voiture->get_vitesse()>$limitation){
+                $infraction = true;
+                $voitureFlashee = $$voiture;
+            }
+        }
+    }
+        
+    
+
+    // echo "\nLa marque de votre voiture est : $voiture1->get_marque()";
 
 
 
