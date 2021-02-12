311c12812b72e2bc5ccaedb547f82ff78f0cb1f2
Consil-Anthony <consil.anthony@outlook.com>
2021-02-12 11:54:24 +0100

    exo poo

--- Anthony_C/Lab/Projet_employer/src/entities/Cadre.class.php
+++ Anthony_C/Lab/Projet_employer/src/entities/Cadre.class.php
@@ -1,29 +1,29 @@
 <?php
-    class Cadre implements Employe{
-        private $employeToManage;
+    class Cadre extends Employe{
+        private $_employeToManage;
 
-        public function __construct($firstName,$lastName,$socialNumber,$salary,$job){
+        public function __construct(string $firstName,string $lastName,int $socialNumber,float $salary,string $job){
             parent :: __construct($firstName,$lastName,$socialNumber,$salary,$job);
         }
 
         public function get_employeToManage(){
-            return $this->$employeToManage;
+            return $this->_employeToManage;
         }
 
         public function set_employeToManage(array $employeToManage){
-            $this->$employeToManage=$employeToManage;
+            $this->_employeToManage=$employeToManage;
         }
 
         public function presentation(){
-            echo "Bonjour, je suis ".$this->$firstName." ".$this->$lastName.", je suis un cadre. Mon numéro de sécu est : ".$this->$socialSecurityNumber." mon salaire est de : ".$this->$salary." et je travail en tant que ".$this->$job.".\nJe manage : .".$this->$employeToManage[0]->get_firstName." ".$this->$employeToManage[0]->get_lastName." et ".$this->$employeToManage[1]->get_firstName." ".$this->$employeToManage[1]->get_lastName.".";
+            echo "Bonjour, je suis ".$this->_firstName." ".$this->_lastName.", je suis un cadre. Mon numéro de sécu est : ".$this->_socialSecurityNumber." mon salaire est de : ".$this->_salary." et je travail en tant que ".$this->_job.".\nJe manage : ".$this->_employeToManage[0]->get_firstName()." ".$this->_employeToManage[0]->get_lastName()." et ".$this->_employeToManage[1]->get_firstName()." ".$this->_employeToManage[1]->get_lastName().".\n\n";
         }
 
         public function augmentation(){
-            echo "Le cadre augmente ".$this->employeToManage[0]->get_firstName." ".$this->employeToManage[0]->get_lastName." de 10% et ".$this->employeToManage[1]->get_firstName." ".$this->employeToManage[1]->get_lastName." de 20%.\n";
-            $this->employeToManage[0]->set_salary($this->employeToManage[0]->get_salary*10/100);
-            $this->employeToManage[1]->set_salary($this->employeToManage[1]->get_salary*20/100);
-            echo "Le salaire de ".$this->employeToManage[0]->get_firstName." ".$this->employeToManage[0]->get_lastName." est maintenant de ".$this->employeToManage[0]->get_salary."€.";
-            echo "Le salaire de ".$this->employeToManage[1]->get_firstName." ".$this->employeToManage[1]->get_lastName." est maintenant de ".$this->employeToManage[1]->get_salary."€.";
+            echo "Le cadre augmente ".$this->_employeToManage[0]->get_firstName()." ".$this->_employeToManage[0]->get_lastName()." de 10% et ".$this->_employeToManage[1]->get_firstName()." ".$this->_employeToManage[1]->get_lastName()." de 20%.\n";
+            $this->_employeToManage[0]->set_salary($this->_employeToManage[0]->get_salary()+($this->_employeToManage[0]->get_salary()*10/100));
+            $this->_employeToManage[1]->set_salary($this->_employeToManage[1]->get_salary()+($this->_employeToManage[1]->get_salary()*10/100));
+            echo "Le salaire de ".$this->_employeToManage[0]->get_firstName()." ".$this->_employeToManage[0]->get_lastName()." est maintenant de ".$this->_employeToManage[0]->get_salary()."€.:\n";
+            echo "Le salaire de ".$this->_employeToManage[1]->get_firstName()." ".$this->_employeToManage[1]->get_lastName()." est maintenant de ".$this->_employeToManage[1]->get_salary()."€.\n";
         }
     }
 ?>
--- Anthony_C/Lab/Projet_employer/src/entities/Employe.class.php
+++ Anthony_C/Lab/Projet_employer/src/entities/Employe.class.php
@@ -1,61 +1,61 @@
 <?php
     class Employe{
-        private $firstName;
-        private $lastName;
-        private $socialSecurityNumber;
-        private $salary;
-        private $job;
+        protected $_firstName;
+        protected $_lastName;
+        protected $_socialSecurityNumber;
+        protected $_salary;
+        protected $_job;
 
-        public function __construct($firstName,$lastName,$socialNumber,$salary,$job){
-            $this->set_firstName();
-            $this->set_lastName();
-            $this->set_socialNumber();
-            $this->set_salary();
-            $this->set_job();
+        public function __construct(string $firstName,string $lastName,int $socialNumber,float $salary,string $job){
+            $this->set_firstName($firstName);
+            $this->set_lastName($lastName);
+            $this->set_socialNumber($socialNumber);
+            $this->set_salary($salary);
+            $this->set_job($job);
         }
 
         public function get_firstName(){
-            return $this->$firstName;            
+            return $this->_firstName;            
         }
 
         public function set_firstName($firstName){
-            $this->$firstName = $firstName;
+            $this->_firstName = $firstName;
         }
 
         public function get_lastName(){
-            return $this->$lastName;            
+            return $this->_lastName;            
         }
 
         public function set_lastName($lastName){
-            $this->$lastName = $lastName;
+            $this->_lastName = $lastName;
         }
 
         public function get_socialNumber(){
-            return $this->$socialSecurityNumber;            
+            return $this->_socialSecurityNumber;            
         }
 
         public function set_socialNumber($socialNumber){
-            $this->$socialSecurityNumber = $socialNumber;
+            $this->_socialSecurityNumber = $socialNumber;
         }
 
         public function get_salary(){
-            return $this->$salary;            
+            return $this->_salary;            
         }
 
         public function set_salary($salary){
-            $this->$salary = $salary;
+            $this->_salary = $salary;
         }
 
         public function get_job(){
-            return $this->$job;            
+            return $this->_job;            
         }
 
         public function set_job($job){
-            $this->$job = $job;
+            $this->_job = $job;
         }
 
         function presentation(){
-            echo "Bonjour, je suis ".$this->$firstName." ".$this->$lastName." mon numéro de sécu est : ".$this->$socialSecurityNumber." mon salaire est de : ".$this->$salary." et je travail en tant que ".$this->$job.".\n";
+            echo "Bonjour, je suis ".$this->_firstName." ".$this->_lastName." mon numéro de sécu est : ".$this->_socialSecurityNumber." mon salaire est de : ".$this->_salary." et je travail en tant que ".$this->_job.".\n\n";
         }
     }
 ?>
